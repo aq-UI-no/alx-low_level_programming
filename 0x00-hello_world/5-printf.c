e539eaa5c88d19e647a6c6ee241e29d73004d367
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-05 12:37:05 -0700

    printf hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * i'll use printf to write a statement
+ * main is the entry point
+ * returns 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
