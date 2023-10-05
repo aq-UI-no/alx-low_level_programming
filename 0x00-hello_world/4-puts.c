ed0ed7167a80c2594e1b9ec6264c6402ab8c73a7
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-05 12:52:08 -0700

    main correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
-
-/*
- *printing exactw= words followed by a new line
+#include<stdio.h>
+/**
+ * main - Entry point
  *
- *main- Entry point
- *
- *Return: 0 (success)
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
