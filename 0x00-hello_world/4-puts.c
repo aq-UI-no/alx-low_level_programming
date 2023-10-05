d573bd3dbc3f7f60195a80eee1c06f2b7eff9065
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-05 12:20:25 -0700

    using puts to print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ *printing exactw= words followed by a new line
+ *
+ *main is the entry point
+ *
+ *return value is 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
