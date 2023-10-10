d0892d167e1e6f02fc7cb7667e3c4203420f5442
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-09 23:27:54 -0700

    made betty corrections

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: 0 Always (success)
- */
+* main - Entry point
+*
+* Return: 0 Always (success)
+*/
 
 int main(void)
 {
-        char letter = 'z';
+char letter = 'z';
 
-        while (letter >= 'a')
-        {
-                putchar(letter);
-                letter--;
-        }
-        putchar('\n');
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
 
-        return (0);
+return (0);
 }
