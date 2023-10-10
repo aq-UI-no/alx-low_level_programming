1d2e85913524e9991cec46c66b67038cd7d85fc7
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-09 17:27:10 -0700

    positive and negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - this is the entry point
+* Return: 0 upon successful execution, non-zero on error
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+printf("The number: %d", n);
+if (n > 0)
+{
+printf("is positive\n");
+}
+else if (n == 0)
+{
+printf("is zero\n");
+}
+else if (n < 0)
+{
+printf("is negative\n");
+}
+printf("\n");
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Learning to use putchar in this directory as well as boolean operations such as if else loop, while loop etc. Will keep this readme updated in due time.
