18ccbcb893c5917ef44fd26c9ea910b37a06c135
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-09 20:20:21 -0700

    using printf to print all digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+	return (0);
+}
+
