5f627faba7ce864ffdb7bb2fd4c0c8a2bc650e29
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-11 23:21:36 -0700

    prints the alphabet in lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - pritns the alphabet 10 times in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+	}
+}
