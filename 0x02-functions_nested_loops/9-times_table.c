573173f84c55d42be57a60ffd2e15419dfc8a04c
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-13 00:40:36 -0700

    puntuation error correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,30 @@ void times_table(void)
 {
 	int i, j, result;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-	for (j = '0'; j <= '9'; j++)
-	{
-		result = 1 * j;
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-		if (j == 0)
-			_putchar(result + '0');
-		else
-		{
-			_putchar(' ');
-			if (result < 10)
-				_putchar(' ');
+			if (j == 0)
+				_putchar(result + '0');
 			else
-				_putchar((result / 10) + '0');
-			_putchar((result % 10) + '0');
+			{
+				_putchar(' ');
+				if (result < 10)
+					_putchar(' ');
+				else
+					_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		if (j < 9)
-		{
-			_putchar(',');
-			_putchar(' ');
+		_putchar('\n');
 	}
-	_putchar('\n');
-	}
-}
 }
