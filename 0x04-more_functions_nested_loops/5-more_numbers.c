816035d8e854355d581216b7de1cc2b7c264e167
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-13 06:35:25 -0700

    prints 10 times the numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - Prints a number as characters
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+if (n > 9)
+{
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+else
+{
+_putchar(n + '0');
+}
+}
+/**
+ * more_numbers - prints 10 times the numbers
+ *
+ */
+void more_numbers(void)
+{
+int i, j;
+
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+print_number(i);
+}
+_putchar('\n');
+}
+}
