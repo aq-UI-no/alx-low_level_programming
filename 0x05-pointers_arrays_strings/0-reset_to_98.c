92a786e3fb4ff523364b494e70fdcea332724410
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-19 13:13:23 -0700

    first pointers assignment

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 - value it points to
+*@n: value it points to
+*Return: 0 always (successful)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ I am learning about pointers, strings and arrays and this directory is where I will be practising and answering my assignments
