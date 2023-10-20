924c1ecbf53895f0840a3a48fb17a54fd2c7b142
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-19 23:45:58 -0700

    made a few corrections

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array - the integer
- *@a: pointer
- *@n: value of space
- *@i: integer
- */
+* print_array - the integer
+*@a: pointer
+*@n: value of space
+*
+*/
 void print_array(int *a, int n)
 {
-	int i;
-for (i = 0; i < n; i++)
-	if (i < (n - 1))
-	printf("%d, ", *(a + i));
-	else
+for (int i = 0; i < n; i++)
+if (i < (n - 1))
+printf("%d, ", *(a + i));
+else
 printf("%d\n", *(a + i));
+}
+/**
+ * main - entry point
+ *Return: 0 Always
+ */
+int main(void)
+{
+int arr[] = {1, 2, 3, 4, 5};
+int n = sizeof(arr) / sizeof(arr[0]);
+
+print_array(arr, n);
+
+return (0);
 }
