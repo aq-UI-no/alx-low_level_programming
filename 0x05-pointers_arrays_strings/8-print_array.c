c69fe4b5b8c5365929c243ae4effb17e7d3b81ea
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-20 04:20:41 -0700

    removed a part of the code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,22 +8,10 @@
 */
 void print_array(int *a, int n)
 {
-for (int i = 0; i < n; i++)
+	int i;
+for (i = 0; i < n; i++)
 if (i < (n - 1))
 printf("%d, ", *(a + i));
 else
 printf("%d\n", *(a + i));
-}
-/**
- * main - entry point
- *Return: 0 Always
- */
-int main(void)
-{
-int arr[] = {1, 2, 3, 4, 5};
-int n = sizeof(arr) / sizeof(arr[0]);
-
-print_array(arr, n);
-
-return (0);
 }
