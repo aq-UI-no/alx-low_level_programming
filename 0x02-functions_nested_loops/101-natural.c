013277cb2ac6d1d76e4567b9f57bd097121309de
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-22 02:59:59 -0700

    prints multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: 0 always
+*/
+int main(void)
+{
+int sum = 0;
+int i;
+
+for (i = 3; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
