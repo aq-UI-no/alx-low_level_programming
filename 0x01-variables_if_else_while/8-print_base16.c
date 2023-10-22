1f8f07fdf82e7d56aa143d1ed4e67bf66ee815f7
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-21 21:40:02 -0700

    made betty corrections

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: 0 always, success
- */
+* main - entry point
+*
+* Return: 0 always, success
+*/
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i <= 15; i++) {
-        if (i < 10) {
-            putchar('0' + i); /* prints digits 0 - 9 */
-        } else {
-            putchar('a' + i - 10); /* prints a-f in lowercase */
-        }
-    }
-    putchar('\n'); /* prints a new line at the end of the line */
-
-    return (0);
+for (i = 0; i <= 15; i++)
+{
+if (i < 10)
+{
+putchar('0' + i); /* prints digits 0 - 9 */
 }
+else
+{
+putchar('a' + i - 10); /* prints a-f in lowercase */
+}
+}
+putchar('\n'); /* prints a new line at the end of the line */
 
+return (0);
+}
