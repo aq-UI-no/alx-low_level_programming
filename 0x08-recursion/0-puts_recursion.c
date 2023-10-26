f78331cfb0ac1a5680d35777144237fbdcd7350e
aq-UI-no <tiffanyaquino116@gmail.com>
2023-10-25 18:07:31 -0700

    made betty corrections

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-if(*s == '\0')
+if (*s == '\0')
 {
-	_putchar('\n');
-	return;
+	_putchar ('\n');
 }
-_putchar(*s);
+_putchar (*s);
 _puts_recursion(s + 1);
 }
