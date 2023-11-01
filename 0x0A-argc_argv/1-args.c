49c0816bc9e58c20d48f950aa8e1f11a26c72792
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-01 13:20:05 -0700

    made corrections on the code

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - entry point
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 always, success
- */
+* main - entry point
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 always, success
+*/
 int main(int argc, char *argv[])
 {
-		int i;
-
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-		return (0);
+(void)argv;
+{
+if (argc >= 1)
+{
+printf("%d\n", argc - 1);
+}
+}
+return (0);
 }
