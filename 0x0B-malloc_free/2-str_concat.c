71cc209cebc3154a8dfe594ea02598c10b0c8dc2
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-03 07:18:25 -0700

    made a memory correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,11 +9,13 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-unsigned int i, j, len;
-char *cat;
+unsigned int i, j, len1, len2;
+char *cat; /*will be the third string */
 
 i = 0;
-len = 0;
+j = 0;
+len1 = 0;
+len2 = 0;
 
 if (s1 == NULL)
 return (NULL);
@@ -21,13 +23,13 @@ return (NULL);
 if (s2 == NULL)
 return (NULL);
 
-while (s1[len] != '\0')
-len++;
+while (s1[len1] != '\0')
+len1++;
 
-while (s2[len] != '\0')
-len++;
+while (s2[len2] != '\0')
+len2++;
 
-cat = malloc(len + 1);
+cat = malloc(sizeof(char) * (len1 + len2 + 1));
 
 if (cat == NULL)
 return (NULL);
