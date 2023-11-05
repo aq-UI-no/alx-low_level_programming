fb809a13d8dabf1a8d7a323c0fa9e2597eb26eee
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-05 06:51:35 -0800

    prints the reverse of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * rev_string-reversese a string
+ * @s: the string to be printed
+ *
+ */
+void rev_string(char *s)
+{
+	int len, i;
+	char *start, *end, temp;
+
+	len = _strlen(s);
+	start = s;
+	end = s;
+
+	for (i = 0; i < len - 1; i++)
+		end++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *end;
+		*end = *start;
+		*start = temp;
+
+		start++;
+		end--;
+	}
+	for (i = 0; i < len; i++)
+			{
+			_putchar(s[i]);
+}
+_putchar('\n');
+}
+/**
+ * _strlen - the lenth of the string
+ *@ptr: the pointer character
+ *
+ * Return: the value of i if successful
+ *
+ */
+int _strlen(char *ptr)
+{
+	int i;
+
+	i = 0;
+
+	while (*(ptr + 1) != '\0')
+		i++;
+	return (i);
+}
