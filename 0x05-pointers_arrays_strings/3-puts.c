dbb0d10cb8fc0330d50284fd6d0986ba7da8d2b5
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-04 17:18:54 -0700

    prints string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts - prints string to stdout
+ *@str: the string to be printed
+ *
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+	_putchar(*str);
+	str++;
+}
+}
