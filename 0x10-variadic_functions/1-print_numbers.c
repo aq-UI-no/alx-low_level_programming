731a3bbd443bbfc2dc7e0ab408550816f3708005
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-11 03:10:52 -0800

    output error fixed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,11 +17,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (counter = 0; counter < n; counter++)
 {
-	printf("%d", va_arg(my_list, unsigned int));
-	if (counter != n - 1 && separator != NULL)
-		printf("%s", separator);
+	if (!separator)
+		printf("%d", va_arg(my_list, unsigned int));
 	else
-		printf("\n");
+		if (counter == 0)
+		printf("%d", va_arg(my_list, unsigned int));
+		else
+		printf("%s%d", separator, va_arg(my_list, unsigned int));
 }
+		printf("\n");
 va_end(my_list);
 }
