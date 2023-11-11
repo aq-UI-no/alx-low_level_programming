a7fc7075bbb015a375fd97716c7451d312e63a24
aq-UI-no <tiffanyaquino116@gmail.com>
2023-11-11 02:23:46 -0800

    prints a string followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+*print_strings - prints string followed by new line
+*@n: number of strings passed to the function
+*@separator: string to be printed between strings
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int counter;
+
+va_list my_list;
+
+va_start(my_list, n);
+
+for (counter = 0; counter < n; counter++)
+{
+char *airport;
+airport = va_arg(my_list, char *);
+
+printf("%s", airport);
+
+if (counter != n - 1)
+printf("%s", separator);
+else
+printf("\n");
+}
+va_end(my_list);
+}
