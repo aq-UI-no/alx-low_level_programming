17fa217d71f73cc963fb78513aa418efeabb90c1
aq-UI-no <tiffanyaquino116@gmail.com>
2024-07-03 03:33:23 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,49 +2,25 @@
 /**
 * rev_string-reversese a string
 * @s: the string to be printed
+* _putchar - prints each character
 *
+* Return: 0 is success
 */
+
 void rev_string(char *s)
 {
-int len, i;
-char *start, *end, temp;
+	int a = 0, b, c;
+	char d;
 
-len = _strlen(s);
-start = s;
-end = s;
-
-for (i = 0; i < len - 1; i++)
-end++;
-
-for (i = 0; i < len / 2; i++)
-{
-temp = *end;
-*end = *start;
-*start = temp;
-
-start++;
-end--;
-}
-for (i = 0; i < len; i++)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
-}
-/**
-* _strlen - the lenth of the string
-*@ptr: the pointer character
-*
-* Return: the value of i if successful
-*
-*/
-int _strlen(char *ptr)
-{
-int i;
-
-i = 0;
-
-while (*(ptr + 1) != '\0')
-i++;
-return (i);
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
 }
